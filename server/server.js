f65e53a1e4079cb291c296a0e094ec809d3053b6
Cengiz Barkın Öztürk <cengiz@Hackbook-Pro.local>
2016-12-22 15:26:59 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ let app = express();
 app.use(bodyParser.json());
 
 
+
 app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text
@@ -28,3 +29,8 @@ app.post('/todos', (req, res) => {
 app.listen(3000, ()=> {
   console.log('Started on port 3000');
 });
+
+
+module.exports = {
+  app
+}
