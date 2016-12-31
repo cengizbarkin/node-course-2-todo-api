9878f88ad24571c710dec305c868aea7046ec10c
Cengiz Barkın Öztürk <cengiz@Hackbook-Pro.local>
2016-12-31 15:51:33 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -99,14 +99,10 @@ app.post('/users', (req, res) => {
 });
 
 
-
-
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-
-// POST users/ Login
 
 app.post('/users/login', (req,res) => {
   let body = _.pick(req.body, ['email', 'password']);
@@ -119,6 +115,14 @@ app.post('/users/login', (req,res) => {
   });
 });
 
+
+app.delete('/users/me/token',authenticate, (req, res) => {
+  req.user.removeToken(req.token).then( () => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 
 app.listen(port, ()=> {
